e9031721e1039f0c9b581ea5740cbe2778aafd81
joshuapole99 <joshuapole@live.nl>
2018-09-26 19:34:28 +0200

    Add files via upload

---
+++ scripts/Grow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grow : MonoBehaviour
+{
+    public Shader NewSurfaceShader;
+  
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+       
+    
+        NewSurfaceShader = Shader.Find("Custom/NewSurfaceShader");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "cube")
+        {
+            rend.material.SetFloat("Extrusion Amount",1f);
+
+            Destroy(col.gameObject);
+        }
+    }
+}
